3a89a6eccad9f80495b2dc03d1bfa02317573019
Rafi Khaled <rkhaled@vassar.edu>
2017-12-14 22:07:06 -0500

    added wordinterface

---
+++ graph_race/Word_Input/src/main/java/WordInterface.java
@@ -0,0 +1,14 @@
+
+/**
+ * Write a description of interface Word here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public interface WordInterface
+{
+    public double[] getScores();
+    
+    public String getWord();
+   
+}
